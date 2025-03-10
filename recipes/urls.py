a3baea539f3d54a32f64c7dbfac934de4e0aa114
sergiolttz <sergiolattanzio05@gmail.com>
2025-03-10 14:45:01 -0400

    recipe detail done

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import recipe_list  # Importa la función recipe_list
+from .views import recipe_list, recipe_detail  # Importa ambas vistas
 
 urlpatterns = [
     path('', recipe_list, name='recipes-list'),
+    path('recipes/<int:pk>/', recipe_detail, name='recipe-detail'), #agrega la url para el detalle
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
 def recipe_list(request):
-    """
-    Vista para mostrar la lista de recetas.
-    """
-    recipes = Recipe.objects.all()  # Obtiene todas las recetas de la base de datos
-    context = {
-        'recipe_list': recipes,  # Pasa la lista de recetas a la plantilla
-    }
-    return render(request, 'recipes-list.html', context)
+    """Vista para mostrar la lista de recetas."""
+    recipe_list = Recipe.objects.all()
+    return render(request, 'recipes-list.html', {'recipe_list': recipe_list})
+
+def recipe_detail(request, pk):
+    """Vista para mostrar el detalle de una receta."""
+    recipe = get_object_or_404(Recipe, pk=pk)    
+    return render(request, 'recipe-detail.html', {'recipe': recipe})
